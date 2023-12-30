b4d318abff9c3e0a53419b1f21e9f76ab0b0268a
Neto test <disr9197@gmail.com>
2023-12-30 11:05:55 +0500

    Creating Two Functions for Reviewer and Lecturers

--- main.py
+++ main.py
@@ -7,6 +7,28 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(self, Reviewer):
+            if course in self.courses_in_progress and lecturer in lecturer.courses_taught:
+                if course in lecturer.grades:
+                    lecturer.grades[course].append(grade)
+                else:
+                    lecturer.grades[course] = [grade]
+            else:
+                return 'Ошибка'
+        else:
+            return 'Ошибка'
+
+    def rate_hw(self, mentor, course, grade):
+        if isinstance(mentor, Reviewer) and course in self.courses_in_progress and course in mentor.courses_attached:
+            if course in self.grades:
+                self.grades[course].append(grade)
+            else:
+                self.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -22,13 +44,16 @@ class Mentor:
         else:
             return 'Ошибка'
 
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_taught = []
+        self.grades = {}
 
     def add_course(self, course):
         self.courses_taught.append(course)
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -37,6 +62,7 @@ class Reviewer(Mentor):
 
     def add_course(self, course):
         self.courses_reviewed.append(course)
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress.append('Python')
@@ -56,6 +82,9 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 9)
 cool_mentor.rate_hw(best_student, 'Python', 8)
 
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+
 print(best_student.grades)
 print(cool_lecturer.courses_taught)
+print(cool_lecturer.grades)
 print(cool_reviewer.courses_reviewed)
